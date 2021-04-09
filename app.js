1c518304dc074f68c5ce33bf0527596d11fbd106
dudvik92 <66221191+dudvik92@users.noreply.github.com>
2021-04-09 13:19:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let app;
 const CORS = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
@@ -9,38 +8,28 @@ export default function appSrc(express, bodyParser, createReadStream, crypto, ht
 {
     const app = express();
     const server = http.createServer(app);
-    const fs = createReadStream;
-    return app;
-}
-
-
-import('./index.js')
-.then(module => {
-    app = module.app;
-    let hash = module.crypto.createHash('sha1');
-    
+    let hash = crypto.createHash('sha1');   
     app
     .use((req, res, next) => res.status(200).set(CORS) && next())
-    .use(module.bodyParser.urlencoded({extended: true}))
+    .use(bodyParser.urlencoded({extended: true}))
     .get(/login/, (req, res) => {res.send('dudvik')})
-    .get(/code/, (req, res) => module.createReadStream(import.meta.url.substring(7)).pipe(res))
+    .get(/code/, (req, res) => createReadStream(import.meta.url.substring(10)).pipe(res))
     .get('/sha1/:input/', r => {
         hash.update(r.params.input);
         r.res.send(hash.digest('hex'))})
     .get('/req/', (req, res) => { 
         let path = req.query.addr;
-        module.http.get(path, (resp) => {
+        http.get(path, (resp) => {
             let rawData = '';
             const {statusCode} = resp;
             resp.setEncoding('utf8');
             resp.on('data', (chunk) => { rawData += chunk; });
             resp.on('end', () => {res.send(rawData);})
             })
-        
         })
     .post('/req', r => {
         let path = r.body.addr;
-        module.http.get(path, (resp) => {
+        http.get(path, (resp) => {
             let rawData = '';
             const {statusCode} = resp;
             resp.setEncoding('utf8');
@@ -49,6 +38,7 @@ import('./index.js')
             })
 
     })
-
     .all(/./, (req, res) => {res.send('dudvik')});
-})
+    return app;
+}
+
