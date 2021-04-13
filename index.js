aac5179ad41ffd4edff414ec5baf7cbd1f80cb25
dudvik92 <66221191+dudvik92@users.noreply.github.com>
2021-04-13 23:12:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import http from 'http';
+import mongo from 'mongodb';
 import {createReadStream} from 'fs';
 import bodyParser from 'body-parser';
 import crypto from 'crypto';
@@ -7,7 +8,7 @@ import crypto from 'crypto';
 
 import appSrc from './app.js';
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http); 
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongo); 
 
 app.listen(process.env.PORT || 3000);
 
